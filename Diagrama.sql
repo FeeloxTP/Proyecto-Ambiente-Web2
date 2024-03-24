19cd36276f099f444110dd15eb63404415dab85b
FeeloxTP <luisfelipetrejos28@gmail.com>
2024-03-23 23:38:24 -0600

    diagrama

---
+++ Diagrama.sql
@@ -0,0 +1,16 @@
+use TiendaNFT;
+
+INSERT INTO Pais (IdPais, Descripcion) VALUES
+('US', 'Estados Unidos'),
+('GB', 'Reino Unido'),
+('CA', 'Canadá'),
+('FR', 'Francia'),
+('DE', 'Alemania'),
+('JP', 'Japón'),
+('BR', 'Brasil'),
+('IN', 'India'),
+('CN', 'China'),
+('CR', 'Costa Rica');
+
+
+select * from Pais
